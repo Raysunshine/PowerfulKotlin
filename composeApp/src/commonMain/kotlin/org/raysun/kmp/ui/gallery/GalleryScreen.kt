d656295feb9d8b291d22d3db86c8d9d72544b765
Raysun.ma <raysunshine990923@163.com>
2024-06-14 11:00:36 +0800

    feat:GalleryScreen finish

--- composeApp/src/commonMain/kotlin/org/raysun/kmp/ui/gallery/GalleryScreen.kt
+++ composeApp/src/commonMain/kotlin/org/raysun/kmp/ui/gallery/GalleryScreen.kt
@@ -1,21 +1,36 @@
 package org.raysun.kmp.ui.gallery
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+import org.raysun.kmp.domain.resp.Galleries
 import org.raysun.kmp.platform.GalleriesFrame
 
 class GalleryScreen : Screen {
@@ -26,33 +41,97 @@ class GalleryScreen : Screen {
 
         val uiState = screenModel.state.collectAsState()
 
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            GalleriesFrame(
-                sideBar = {
-                    Button(
-                        onClick = {}
-                    ) {
-                        Text("Icons1")
-                    }
-
-                    Button(
-                        onClick = {}
-                    ) {
-                        Text("Icons2")
-                    }
-                },
-                splitContent = {
-                    Spacer(modifier = Modifier.height(20.dp).fillMaxWidth().background(Color.Blue))
+        GalleriesFrame(
+            sideBar = {
+                Button(
+                    onClick = {}
+                ) {
+                    Text("Icons1")
                 }
-            ) { bodyModifier ->
-                LazyColumn(modifier = bodyModifier) {
-                    items(50) {
-                        Text("Hello $it")
-                    }
+
+                Button(
+                    onClick = {}
+                ) {
+                    Text("Icons2")
+                }
+            },
+        ) { bodyModifier ->
+            AnimatedContent(
+                uiState.value.picList.isNotEmpty(),
+                modifier = bodyModifier,
+            ) { objectsAvailable ->
+                if (objectsAvailable) {
+                    ObjectGrid(
+                        objects = uiState.value.picList,
+                        onObjectClick = { objectId ->
+                            println("OnObjectClick -> $objectId")
+                        }
+                    )
+                } else {
+                    EmptyScreenContent(Modifier.fillMaxSize())
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun ObjectGrid(
+    objects: List<Galleries>,
+    onObjectClick: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(180.dp),
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(objects, key = { it.objectID ?: 0 }) { obj ->
+            ObjectFrame(
+                obj = obj,
+                onClick = { onObjectClick(obj.objectID ?: 0) },
+            )
+        }
+    }
+}
+
+@Composable
+private fun ObjectFrame(
+    obj: Galleries,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier
+            .padding(8.dp)
+            .clickable { onClick() }
+    ) {
+        KamelImage(
+            resource = asyncPainterResource(data = obj.primaryImageSmall ?: ""),
+            contentDescription = obj.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .background(Color.LightGray),
+        )
+
+        Spacer(modifier = Modifier.height(2.dp))
+
+        Text(obj.title ?: "", style = MaterialTheme.typography.subtitle1.copy(fontWeight = FontWeight.Bold))
+        Text(obj.artistDisplayName ?: "", style = MaterialTheme.typography.body2)
+        Text(obj.objectDate ?: "", style = MaterialTheme.typography.caption)
+    }
+}
+
+@Composable
+fun EmptyScreenContent(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        Text("No data available")
     }
 }
